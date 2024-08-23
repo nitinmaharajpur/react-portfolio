e3a8b0257ab316c604aa5075f70e18308abbaab5
Nitin Kumar <nitimaharajpur@gmail.com>
2024-08-24 01:49:17 +0530

    some changes

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,9 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@components': '/src/components',
-    },
-  },
   plugins: [react()],
-});
+})
+
+
